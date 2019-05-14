3409eab222494289bd345aba532981fbd37e88a3
Jonathan Chow <john@xen.net>
2019-05-14 19:36:19 +0800

    add images

--- webpack.config.js
+++ webpack.config.js
@@ -49,6 +49,12 @@ module.exports = {
         ]
       },
       {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         use: ['babel-loader', 'eslint-loader'],
